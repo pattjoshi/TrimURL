ff5106c6a15fcfc489b935b22b75a9c3ccaea182
pattjoshi <omprakashpattjoshi123@gmail.com>
2025-06-07 12:36:35 +0530

    fix: test husky lint-staged1

--- frontend/src/__tests__/Hello.test.jsx
+++ frontend/src/__tests__/Hello.test.jsx
@@ -4,7 +4,7 @@ import { render, screen } from "@testing-library/react";
 import Hello from "../Hello";
 import { describe, expect, it } from "vitest";
 
-describe("Hello Component 1", () => {
+describe("Hello Component ", () => {
   it("renders Hello world heading", () => {
     render(<Hello />);
     expect(screen.getByText("Hello world")).toBeInTheDocument();
